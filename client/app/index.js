14fde7fffc4cd3fb0a89f7a7cb246607902efe08
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2020-05-23 18:38:09 -0700

    setup babel & react

---
+++ client/app/index.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * index.js
+ * This is the entry file for the client applicaiton
+ */
+
+import 'core-js';
+import 'regenerator-runtime/runtime';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(<div>React Works!</div>, rootElement);
